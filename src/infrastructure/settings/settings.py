f26ee1513aac567cc0a526085baa3a36f80b51bd
Alireza Safari <alireza.safaree@gmail.com>
2023-07-19 14:16:03 +0330

    feat(settings): removes default_auto_field from settings so models inherit BaseModel

--- src/infrastructure/settings/settings.py
+++ src/infrastructure/settings/settings.py
@@ -134,8 +134,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 # Rest Framework
 REST_FRAMEWORK = {
 
