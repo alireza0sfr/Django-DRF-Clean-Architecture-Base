065de2db8702c0e7d5bce531bd1002e34dcee684
Alireza Safari <alireza.safaree@gmail.com>
2023-08-18 17:44:09 +0330

    feat(src): adds debugpy

--- src/manage.py
+++ src/manage.py
@@ -2,11 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.conf import settings
+import logging
 
+logger = logging.getLogger(__name__)
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "infrastructure.settings.settings")
+    
+    if settings.DEBUG and (os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN')):
+        import debugpy
+        debugpy.listen(("0.0.0.0", 5678))
+        logger.info('DEBUGGER Attached!')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
