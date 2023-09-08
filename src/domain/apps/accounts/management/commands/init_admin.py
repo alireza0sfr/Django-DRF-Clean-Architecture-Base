42f2bfa175f79fcc74f97b9f3232c8811a51c476
Alireza Safari <alireza.safaree@gmail.com>
2023-09-08 18:24:00 +0330

    feat(domain): adds init_admin command

---
+++ src/domain/apps/accounts/management/commands/init_admin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from decouple import config
+
+User = get_user_model()
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if User.objects.filter(is_superuser=True).count() == 0:
+            User.objects.create_superuser(
+            username=config('SUPERUSER_USERNAME'), password=config('SUPERUSER_PASSWORD'))
+            print('Admin user created!')
+
+        else:
+            print('Admin accounts can only be initialized if no admin accounts exists!')
