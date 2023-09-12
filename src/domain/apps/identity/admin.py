e4be8e8414c4f52db32ec0b803b251e650179ab3
Alireza Safari <alireza.safaree@gmail.com>
2023-09-13 00:05:48 +0330

    feat(domain): adds custom userchange and useradd forms

--- src/domain/apps/identity/admin.py
+++ src/domain/apps/identity/admin.py
@@ -2,13 +2,57 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.utils import timezone
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.forms import ModelForm, CharField, PasswordInput
+
+from infrastructure.exceptions.exceptions import PasswordMissmatchException
 
 from .models import UserBan, IPBan
 
 User = get_user_model()
 
+class UserCreationForm(ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+
+    password1 = CharField(label="Password", widget=PasswordInput)
+    password2 = CharField(label="Password confirmation", widget=PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'is_active', 'is_verified', 'is_superuser', 'is_staff', 'is_hidden']
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise PasswordMissmatchException()
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class UserChangeForm(ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'is_active', 'is_verified', 'is_superuser', 'is_staff', 'is_hidden']
 
 class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
     model = User
     list_display = (
         'id', 'username', 'email', 'last_login', 'date_joined', 'last_used_ip', 'is_staff', 'is_active', 'is_superuser',
