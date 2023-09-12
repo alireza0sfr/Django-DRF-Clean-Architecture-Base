17f26b07afe44416997341bf1b5aaefed2b3d7e5
Alireza Safari <alireza.safaree@gmail.com>
2023-09-13 00:14:10 +0330

    fix(presentation): removes CustomAuthenticationViewset for djoser

--- src/presentation/api/authentication/urls.py
+++ src/presentation/api/authentication/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView, TokenObtainPairView
 
-from presentation.controllers.identity.views import AuthenticationViewSet
-
-
-router = DefaultRouter()
-router.register('users', AuthenticationViewSet, basename="users")
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('djoser.urls')),
     path('jwt/generate/', TokenObtainPairView.as_view(), name='jwt-generate'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify')
--- src/presentation/controllers/identity/views.py
+++
@@ -1,7 +0,0 @@
-from djoser.views import UserViewSet
-
-from infrastructure.repositories.identity.user import UserRepository
-
-
-class AuthenticationViewSet(UserViewSet):
-    queryset = UserRepository().get_all()
