22160fe2a7a18dafe5516bd8a1671a7b54c51aa9
Alireza Safari <alireza.safaree@gmail.com>
2023-09-14 01:48:04 +0330

    refactor(domain): changes default is_active of user to False

--- src/domain/apps/identity/models.py
+++ src/domain/apps/identity/models.py
@@ -30,7 +30,7 @@ class User(BaseModel, AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
