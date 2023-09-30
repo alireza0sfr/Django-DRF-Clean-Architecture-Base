6f545a9d2a680f5c0f924a182c646d5f46466f76
Alireza Safari <alireza.safaree@gmail.com>
2023-09-30 23:58:32 +0330

    refactor(infrastructure): removes TokenAuthentication from rest_framework

--- src/infrastructure/settings/settings.py
+++ src/infrastructure/settings/settings.py
@@ -161,7 +161,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
