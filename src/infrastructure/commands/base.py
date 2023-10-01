e93c3c6f4162ef1f98e06d2f9ea6c28d944ec8a8
Alireza Safari <alireza.safaree@gmail.com>
2023-10-01 11:38:19 +0330

    refactor(infrastructure): removes redundant Validator

--- src/infrastructure/commands/base.py
+++ src/infrastructure/commands/base.py
@@ -3,13 +3,11 @@ from django.db.models.query import Q
 from application.dtos.base import BaseDto
 from application.interfaces.commands.base import IBaseCommand
 from infrastructure.handlers.base import BaseHandler
-from infrastructure.validators.validators import Validator, VNotEmpty
 from infrastructure.services.dto import DtoService
 
 
 class BaseCommand(IBaseCommand):
     handler = None
-    validator: Validator = Validator
     Dto: BaseDto
 
     def __init__(self):
@@ -32,13 +30,8 @@ class BaseCommand(IBaseCommand):
         return handler.create(dto, serialize=serialize)
 
     def retrieve(self, pk=None, serialize=True):
-        validator: Validator = self.validator()
-        validator_roles = {
-            'pk': [VNotEmpty]
-        }
         handler: BaseHandler = self.handler()
-        validator.validate({'pk': pk}, validator_roles)
-        return handler.get(pk, serialize=serialize)
+        return handler.get(pk=pk, serialize=serialize)
 
     def update(self, data, serialize=True):
         dto = self.cast_dto(data)
@@ -50,10 +43,5 @@ class BaseCommand(IBaseCommand):
         return handler.partial_update(pk=pk, data=data, serialize=serialize)
 
     def destroy(self, pk=None):
-        validator: Validator = self.validator()
-        validator_roles = {
-            'pk': [VNotEmpty]
-        }
-        validator.validate({'pk': pk}, validator_roles)
         handler: BaseHandler = self.handler()
         return handler.delete(Q(id=pk))
