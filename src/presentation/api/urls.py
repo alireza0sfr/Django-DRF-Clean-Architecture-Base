4165fbce05c4c1ce5b21c78c1b498823bfb22d91
Alireza Safari <alireza.safaree@gmail.com>
2023-10-24 23:14:14 +0330

    fix(docker): fixes static and media volumes

--- src/presentation/api/urls.py
+++ src/presentation/api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from decouple import config
 
@@ -43,5 +44,8 @@ production_urls = [
 
 if settings.DEBUG:
     urlpatterns += development_urls
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 else:
     urlpatterns += production_urls
