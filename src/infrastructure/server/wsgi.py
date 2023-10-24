0086011054c8ecece177ce188e8a1119576262ea
Alireza Safari <alireza.safaree@gmail.com>
2023-10-24 14:12:11 +0330

    fix(infrastructure): fixes asgi/wsgi no module names 'src'

--- src/infrastructure/server/wsgi.py
+++ src/infrastructure/server/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.infrastructure.settings.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "infrastructure.settings.settings")
 
 application = get_wsgi_application()
