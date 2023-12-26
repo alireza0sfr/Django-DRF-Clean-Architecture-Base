fd10ff350cb8e2c41855782568a6be52632099bb
Alireza Safari <alireza.safaree@gmail.com>
2023-12-26 15:51:10 +0330

    fix(infrastructure): catches python raised exceptions in custom_exception_handler

--- src/infrastructure/exceptions/custom_handlers.py
+++ src/infrastructure/exceptions/custom_handlers.py
@@ -1,14 +1,25 @@
-
 from rest_framework.views import exception_handler
 from infrastructure.exceptions.exceptions import BaseCustomException
+from rest_framework.response import Response
+from rest_framework import status
 
 
 def custom_exception_handler(exc, context):
-
     response = exception_handler(exc, context)
 
     if response is not None and isinstance(exc, BaseCustomException):
-        response.data['errors'] = exc.errors
-        response.data['key'] = exc.key
+        response.data["errors"] = exc.errors
+        response.data["key"] = exc.key
+
+    # Handle Python exceptions
+    elif isinstance(exc, Exception):
+        response = Response(
+            data={
+                "errors": exc.args,
+                "message": "Internal Server Error!",
+                "key": "internal_server_error",
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
     return response
