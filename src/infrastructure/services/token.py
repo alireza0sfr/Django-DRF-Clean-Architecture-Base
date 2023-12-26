61acb04f6543672e95c98dea0737527c66a472f3
Alireza Safari <alireza.safaree@gmail.com>
2023-12-26 15:08:55 +0330

    fix(infrastructure): adds try catch to TokenService decode

--- src/infrastructure/services/token.py
+++ src/infrastructure/services/token.py
@@ -5,17 +5,25 @@ import jwt
 from domain.apps.identity.models import User
 from application.interfaces.services.token import ITokenService
 from infrastructure.serializers.identity.serializers import UserModelSerializer
+from infrastructure.exceptions.exceptions import InvalidTokenException
+
 
 class TokenService(ITokenService):
-
     def decode(self, token: str) -> dict:
-        return jwt.decode(token, settings.SECRET_KEY, settings.SIMPLE_JWT.get('algorithm') or 'HS256')
+        try:
+            return jwt.decode(
+                token,
+                settings.SECRET_KEY,
+                settings.SIMPLE_JWT.get("algorithm") or "HS256",
+            )
+        except Exception as e:
+            raise InvalidTokenException()
 
     def generate(self, user: User) -> dict:
         refresh = RefreshToken.for_user(user)
 
         return {
-            'refresh_token': str(refresh),
-            'access_token': str(refresh.access_token),
-            'user': UserModelSerializer(user).data
+            "refresh_token": str(refresh),
+            "access_token": str(refresh.access_token),
+            "user": UserModelSerializer(user).data,
         }
